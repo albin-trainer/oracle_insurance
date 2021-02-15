bd57d6fff8e4fef62e14163b26f109ce753b06e3
Albin <albinxavier@gmail.com>
2021-02-15 17:55:34 +0530

    fun msg

--- JavaProject/src/com/oracle/helloworld/HelloWorld.java
+++ JavaProject/src/com/oracle/helloworld/HelloWorld.java
@@ -4,8 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a=10;
+		
 System.out.println("Hello World ...");
+String msg="Git hub is fun..";
 System.out.println("Hv a wonderful evening ..");
+System.out.println(msg);
 	}
 
 }
